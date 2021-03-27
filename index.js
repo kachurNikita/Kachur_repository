0b622c99f8257f19916e68c585e3cf915da619ee
Nikita-Kachur <ashtonashtonkachur@gmail.com>
2021-03-27 17:09:40 +0200

    first version

---
+++ index.js
@@ -0,0 +1,10 @@
+"use strict";
+
+
+
+
+let anchors = document.querySelectorAll('.anchors')
+
+anchors.forEach(item => {
+    item.style.backgroundColor = 'purple'
+});
---
+++ main.js
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+     <!-- Head -->  
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/main.css">
+    <title>Main</title>
+</head>
+<!-- Header -->
+<!-- Body -->
+<body> 
+    <header>
+        <div class="contaienr">
+            <div class="anchors">
+                <a href="./index.html" title="Home">Home</a>
+                <a href="./pages/about.html" title="About Us">About Page</a>
+                <a href="./pages/contacts.html" title="Contact Us">Contact Us</a>
+            </div>
+        </div>
+    <h1 title="MyHeader 1">Header 1</h1>
+    <h2 title="MyHeader2">Header 2</h2>
+    <h3 title="MyHeader3">Header 3</h3>
+    <h4 title="MyHeader4">Header 4</h4>
+    <h5 title="MyHeader5">Header 5</h5>
+    <h6 title="MyHeader6">Header 6</h6>
+    <p>fdfdfd</p>
+    <button disabled>Click tis button</button>
+    <img src="./img/grut.jpeg" alt="picture" width="100px" height="100px" >
+    <div class="div">
+        <ol>
+            <li>Create
+                <ul>
+                    <li>mkdir, touch</li>
+                </ul>
+            </li>
+            <li>Read
+                <ul>
+                    <li>cat, ls, nano</li>
+                </ul>
+            </li>
+            <li>Update
+                <ul>
+                    <li>nano, echo"" >> file</li>
+                </ul>
+            </li>
+            <li>Delete
+                <ul>
+                    <li>rm, rmdir -r</li>
+                </ul>
+            </li>
+        </ol>
+    </div>
+    <a href="https://developer.mozilla.org/ru/docs/Web/HTML/Element/base" title="MDN">MDN</a>
+
+    <p>Lorem <bold>ipsum dolor</bold> <italic>sit amet</italic> s<strong>adipisicing</strong>  <em>elit.</em> Et, quisquam.</p>
+    </header>
+    <footer></footer>
+    <script src="."></script>
+</body>
+</html>
